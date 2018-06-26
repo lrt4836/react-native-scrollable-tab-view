b2bd03b850ae00932e1e88176649dee0c0637187
lrt4836 <296659110@qq.com>
2018-06-26 15:46:24 +0800

    1、修复scrollView下划线不显示、不居中的bug

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -60,6 +60,7 @@ const ScrollableTabBar = createReactClass({
   },
 
   updateView(offset) {
+    if (offset.value === undefined) { offset.value = 0 }
     const position = Math.floor(offset.value);
     const pageOffset = offset.value % 1;
     const tabCount = this.props.tabs.length;
@@ -148,7 +149,7 @@ const ScrollableTabBar = createReactClass({
   measureTab(page, event) {
     const { x, width, height, } = event.nativeEvent.layout;
     this._tabsMeasurements[page] = {left: x, right: x + width, width, height, };
-    this.updateView({value: this.props.scrollValue._value, });
+    this.updateView({value: this.props.scrollValue.__getValue(), });
   },
 
   render() {
@@ -207,12 +208,12 @@ const ScrollableTabBar = createReactClass({
       width = WINDOW_WIDTH;
     }
     this.setState({ _containerWidth: width, });
-    this.updateView({value: this.props.scrollValue._value, });
+    this.updateView({value: this.props.scrollValue.__getValue(), });
   },
 
   onContainerLayout(e) {
     this._containerMeasurements = e.nativeEvent.layout;
-    this.updateView({value: this.props.scrollValue._value, });
+    this.updateView({value: this.props.scrollValue.__getValue(), });
   },
 });
 
